692d127cad3570be5457abb5b29905d1c032ed1b
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-01 12:32:05 +0600

    fixing height in book

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,10 +1,11 @@
+import { CiStar } from "react-icons/ci";
 import { NavLink } from "react-router-dom";
 
 const Book = ({ book }) => {
   const { author, bookName, image, category, rating, tags, bookId } = book;
   return (
     <NavLink to={`/books/${bookId}`}>
-      <div className="border-1 border-[#dcdcdc] rounded-lg p-6 flex flex-col">
+      <div className="border-1 border-[#dcdcdc] rounded-lg p-6 flex flex-col ">
         <div className="bg-[#f3f3f3] py-[32px] rounded-lg">
           <img className="w-[134px] h-[192px]  mx-auto" src={image} alt="" />
         </div>
@@ -19,19 +20,17 @@ const Book = ({ book }) => {
             </li>
           ))}
         </ul>
-        <div className="flex-1">
-          <h3 className="text-2xl font-bold my-3">{bookName}</h3>
-          <p style={{ fontFamily: "Work Sans, sans-serif" }}>By:{author}</p>
-          <div className="h-[1px] border-t-2 border-dashed border-[#e6e6e6] my-3"></div>
-          <div
-            style={{ fontFamily: "Work Sans, sans-serif" }}
-            className="flex items-center justify-between mb-2"
-          >
-            <h3>{category}</h3>
-            <div className="flex gap-2 items-center">
-              <p>{rating}</p>
-              <CiStar className="text-[20px] cursor-pointer" />
-            </div>
+        <h3 className="text-2xl font-bold my-3 h-[50px]">{bookName}</h3>
+        <p style={{ fontFamily: "Work Sans, sans-serif" }}>By:{author}</p>
+        <div className="h-[1px] border-t-2 border-dashed border-[#e6e6e6] my-3"></div>
+        <div
+          style={{ fontFamily: "Work Sans, sans-serif" }}
+          className="flex items-center justify-between mb-2"
+        >
+          <h3>{category}</h3>
+          <div className="flex gap-2 items-center">
+            <p>{rating}</p>
+            <CiStar className="text-[20px] cursor-pointer" />
           </div>
         </div>
       </div>
